af5bf29a723960adf7e609a0c88a4a1cc24f3164
otmanzt <otmanzt@gmail.com>
2017-03-13 13:47:11 +0100

    implementacion recursiva

--- factorial.java
+++ factorial.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class factorial {
 	
+	public static long fact(long num) {
+		if (num<=0) {
+			return 1;
+		} else {
+			return num*fact(num-1);
+		}
+	}
+
+	
 	public static void main(String []args) {
 		Scanner scanInput = new Scanner(System.in);
 		System.out.print("Introduzca un numero: ");
